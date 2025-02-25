7c5f3423867e0d7dec97e8b6c247462e8077da5d
Stan Choi <sjc591@nyu.edu>
2025-02-25 15:26:59 -0500

    Add IIS Config

--- BaseballECommerce/Extensions/ServiceExtensions.cs
+++ BaseballECommerce/Extensions/ServiceExtensions.cs
@@ -1,16 +1,19 @@
-﻿namespace BaseballECommerce.Extensions
+﻿namespace BaseballECommerce.Extensions;
+
+public static class ServiceExtensions
 {
-    public static class ServiceExtensions
+    public static void ConfigureCors(this IServiceCollection services) =>
+    services.AddCors(options =>
     {
-        public static void ConfigureCors(this IServiceCollection services)
-        {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy", builder =>
-                    builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader());
-            });
-        }
-    }
+        options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+    });
+
+    public static void ConfigureIISIntegration(this IServiceCollection services) =>
+    services.Configure<IISOptions>(options =>
+    {
+
+    });
 }
