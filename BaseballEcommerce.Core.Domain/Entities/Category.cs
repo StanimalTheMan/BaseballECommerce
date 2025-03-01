a83dd064f6c9061ce6f8bc2a572447c7c17b9d07
Stan Choi <sjc591@nyu.edu>
2025-02-28 19:48:13 -0500

    create category and product entities

---
+++ BaseballEcommerce.Core.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaseballEcommerce.Core.Domain.Entities;
+
+public class Category
+{
+    [Column("CategoryId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Category name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string? Name { get; set; }
+
+    public ICollection<Product>? Products { get; set; }
+}
---
+++ BaseballEcommerce.Core.Domain/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaseballEcommerce.Core.Domain.Entities;
+
+public class Product
+{
+    [Column("ProductId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Product name is a required field.")]
+    [MaxLength(100, ErrorMessage = "Maximum length for the Name is 100 characters.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Product description is a required field.")]
+    public string Description { get; set; }
+
+    [Required]
+    [Display(Name = "Product Price")]
+    [Range(1, 1000)]
+    public double ListPrice { get; set; }
+
+    [ForeignKey(nameof(Category))]
+    public Guid CategoryId { get; set; }
+    public Category? Category { get; set; }
+
+    // url: how to store images of products? as string urL?
+}
