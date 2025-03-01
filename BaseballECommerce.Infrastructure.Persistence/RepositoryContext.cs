264679fd57fb2d43219bbf9d59cab4b331e6769a
Stan Choi <sjc591@nyu.edu>
2025-03-01 09:45:08 -0500

    add

---
+++ BaseballECommerce.Infrastructure.Persistence/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using BaseballEcommerce.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseballECommerce.Infrastructure.Persistence;
+
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+}
---
+++ BaseballECommerce/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace BaseballECommerce.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContextFactory CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = 
+    }
+}
