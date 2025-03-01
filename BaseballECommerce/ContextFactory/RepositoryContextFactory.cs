9cfdf89376f53f2808ce7a559078e2493a788683
Stan Choi <sjc591@nyu.edu>
2025-03-01 10:05:19 -0500

    Add DatabaseCreation migration

--- BaseballECommerce/ContextFactory/RepositoryContextFactory.cs
+++ BaseballECommerce/ContextFactory/RepositoryContextFactory.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore.Design;
+using BaseballECommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace BaseballECommerce.ContextFactory;
 
 public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
 {
-    public RepositoryContextFactory CreateDbContext(string[] args)
+    public RepositoryContext CreateDbContext(string[] args)
     {
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json")
             .Build();
 
-        var builder = 
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+                b => b.MigrationsAssembly("BaseballECommerce.Infrastructure.Persistence"));
+
+        return new RepositoryContext(builder.Options);
     }
 }
